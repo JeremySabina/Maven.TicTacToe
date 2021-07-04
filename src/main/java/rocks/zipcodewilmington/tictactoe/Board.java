f9f77050e748f919190312c545abf5a8a698818f
Jeremy <jeremysabina@gmail.com>
2021-07-04 19:53:10 -0400

    Had  alot of help. Almost done. need to get winner

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,90 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        int countO = 0;
+        int countX = 0;
+        Character[][] board = this.matrix;
+
+        for (int nextRowIndex = 0; nextRowIndex < board.length; nextRowIndex++) {
+            for (int nextColumnIndex = 0; nextColumnIndex < board.length; nextColumnIndex++) {
+                if (board[nextRowIndex][nextColumnIndex] == 'X') {
+                    countX++;
+                } else if (board[nextRowIndex][nextColumnIndex] == 'O') {
+                    countO++;
+                }
+            }
+        }
+        return countO < countX;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        int countO = 0;
+        int countX = 0;
+        Character[][] board = this.matrix;
+
+        for (int nextRowIndex = 0; nextRowIndex < board.length; nextRowIndex++) {
+            for (int nextColumnIndex = 0; nextColumnIndex < board.length; nextColumnIndex++) {
+                if (board[nextRowIndex][nextColumnIndex] == 'X') {
+                    countX++;
+                } else if (board[nextRowIndex][nextColumnIndex] == 'O') {
+                    countO++;
+                }
+            }
+        }
+        return countO > countX;
+
     }
 
     public Boolean isTie() {
-        return null;
+
+        int countO = 0;
+        int countX = 0;
+        Character[][] board = this.matrix;
+
+        for (int nextRowIndex = 0; nextRowIndex < board.length; nextRowIndex++) {
+            for (int nextColumnIndex = 0; nextColumnIndex < board.length; nextColumnIndex++) {
+                if (board[nextRowIndex][nextColumnIndex] == 'X') {
+                    countX++;
+                } else if (board[nextRowIndex][nextColumnIndex] == 'O') {
+                    countO++;
+                }
+            }
+        }
+        return countO == countX;
     }
+
 
     public String getWinner() {
-        return null;
-    }
+        String winnerO = "";
+        int countO = 0;
+        int countX = 0;
+        Character[][] board = this.matrix;
+
+        for (int nextRowIndex = 0; nextRowIndex < board.length; nextRowIndex++) {
+            for (int nextColumnIndex = 0; nextColumnIndex < board.length; nextColumnIndex++) {
+                if (board[nextRowIndex][nextColumnIndex] == 'X') {
+                    countX++;
+                } else if (board[nextRowIndex][nextColumnIndex] == 'O') {
+                    countO++;
+                }
+
+            }
+        }if (countO > countX) {
+            winnerO = "O is the winner!";
+
+    } else return ("X is the winner!");
+
 
 }
+
+
+
